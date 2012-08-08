23e435d9eb8ba404099288a1a72a206e5391e29f
student <student@codeacademy.org>
2012-08-08 14:20:03 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hamburger::Application.config.secret_token = '3524546b7304a2f74c33e6dcc349ff4d7671669187227acacd90494624eaeea885fd704132920e1d0d522941ca75e9e1dc78885a0074b61d56fc15d7f1ca0316'
